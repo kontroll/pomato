6163c9fd485c7acbf4531c8364b4a7b256d718a6
kontroll <johan@hypercrate.com>
2021-09-24 08:55:50 +0200

    Make sure Pomato doesn't break when given invalid command line parameters.

--- pomato.py
+++ pomato.py
@@ -4,9 +4,13 @@ import curses
 import time
 import sys
 import os
+from shutil import which
 
-kwargs          = {sys.argv[i]: sys.argv[i + 1] for i in range(1, len(sys.argv), 2)}
-stage           = 0
+try:
+    kwargs          = {sys.argv[i]: sys.argv[i + 1] for i in range(1, len(sys.argv), 2)}
+except:
+    sys.exit("Usage: pomato.py [-w 25] [-p 5] [-l 15]")
+
 work_duration   = int(kwargs.get("-w", "25"))*60+1 # We're adding the extra seconds just for appearances' sake
 duration        = work_duration
 pause           = int(kwargs.get("-p", "5" ))*60+1
@@ -14,7 +18,8 @@ long_pause      = int(kwargs.get("-l", "15"))*60+1
 time_format     = "%M:%S"
 
 def main(screen):
-    global duration, stage
+    global duration
+    stage = 0
     message = "Zug zug!"
     xpad = curses.COLS//2-17
     ypad = curses.LINES//2-2
@@ -25,17 +30,19 @@ def main(screen):
     while duration:
         duration -= 1
         screen.clear()
-        screen.addstr(1, 1, message)
-        screen.addstr(ypad, 0, print_timestamp(time.strftime(time_format, time.gmtime(duration)), 1, xpad))
+        screen.addstr(1, 2, message)
+        screen.addstr(ypad, 0, draw_timestamp(time.strftime(time_format, time.gmtime(duration)), 1, xpad))
         screen.refresh()
 
         time.sleep(1)
 
         while not duration:
-            os.system("paplay " + os.path.dirname(os.path.realpath(__file__)) + "/alert.ogg")
-            if screen.getkey() == "q":
+            if which("paplay"): # Let's not just assume every system has `paplay`
+                os.system("paplay " + os.path.dirname(os.path.realpath(__file__)) + "/alert.ogg")
+            action = screen.getkey()
+            if action == "q":
                 break
-            elif screen.getkey() == " ":
+            else:
                 stage +=1
                 if   stage in [1, 3, 5]:
                     duration = pause
@@ -63,7 +70,7 @@ number = [ # Stolen from tty-clock -- Could have been widened programmatically.
      ["   ", " █ ", "   ", " █ ", "   "], # :-backup
 ]
 
-def print_timestamp(timestamp="00:00", padding=0, lpadding=0):
+def draw_timestamp(timestamp="00:00", padding=0, lpadding=0):
     rval = ""
     for i in range(5):
         rval += " "*lpadding
